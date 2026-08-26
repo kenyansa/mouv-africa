a516b95beb5c06f39abca302d9685f22f871a26f
Justine Kenyansa <kenyansajustine@gmail.com>
2026-08-26 17:52:24 +0300

    final touches

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  server: {
+    proxy: {
+      '/api/core': {
+        target: 'https://app.mconnect.africa',
+        rewrite: (path) => path.replace(/^\/api\/core/, '/core'),
+        changeOrigin: true,
+      },
+    },
+  },
+});
